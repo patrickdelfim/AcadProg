e55832da619cd7f5a3c05c0bb135bd63e7b13a9c
Joao Bartolot (windows) <jvcbartolot@gmail.com>
2019-11-05 00:33:27 -0300

    [10] - Corrigir Validação de CPF

--- sources/aluno.c
+++ sources/aluno.c
@@ -18,18 +18,20 @@ void cadastroAluno(struct aluno cadastro) {
                 break;
             } else if (validarCPF(cadastro.cpf) == 0) {
                 corTexto("CPF Invalido porfavor digite novamente\n", 'r');
+                getchar();
             } else if (validarCPF(cadastro.cpf) == 2) {
                 corTexto("O CPF deve conter 11 numeros\n", 'r');
             } else if (validarCPF(cadastro.cpf) == 3) {
                 corTexto("O CPF nao contem letras\n", 'r');
+                getchar();
             } else if (validarCPF(cadastro.cpf) == 4) {
                 corTexto("O CPF nao pode conter numeros iguais\n", 'r');
+                getchar();
             }
 
             printf("Por favor insira as informacoes do aluno:\n\n");
 
             printf("CPF (apenas numeros): ");
-            getchar();
             fgets(cadastro.cpf, 12, stdin);
         }
     }
@@ -46,14 +48,15 @@ void cadastroAluno(struct aluno cadastro) {
     fgets(cadastro.nome, 100, stdin);
     while (strlen(cadastro.nome) < 3) {
         system(clear);
+        corTexto("O nome deve conter pelo menos 3 letras\n", 'r');
 
         printf("Por favor insira as informacoes do aluno:\n\n");
 
         printf("CPF (apenas numeros): ");
         corTexto(cadastro.cpf, 'g');
         printf("\n");
-        getchar();
         printf("Nome: ");
+        getchar();
         fgets(cadastro.nome, 100, stdin);
     }
 }
--- sources/main.c
+++ sources/main.c
@@ -7,8 +7,6 @@
 #include "../lib/professor.h"
 #include "../lib/validadores.h"
 
-/* void cadastroAluno(struct aluno *cadastro); */
-
 int main() {
     char input;
 
@@ -22,7 +20,9 @@ int main() {
 
             cadastroAluno(alunoCadastrado);
 
-            printf("%s\n", alunoCadastrado.cpf);
+            // printf("%s\n", alunoCadastrado.cpf);
+            // printf("%s\n", alunoCadastrado.nome);
+            // printf("%d\n", strlen(alunoCadastrado.nome)); // Adicionar um realloc no nome do aluno
         }
         if (input == '2') {
             struct professor profCadastrado;
@@ -30,41 +30,9 @@ int main() {
             profCadastrado.cpf = malloc(sizeof(char) * 11);
 
             cadastroProfessor(profCadastrado);
-
-            printf("%s\n", profCadastrado.cpf);
         }
 
     } while (input != 'x' && input != 'X');
 
-    /* struct aluno aluno1; */
-
-    /* cadastroAluno(&aluno1); */
-
-    /* printf("O nome do aluno eh %s\n",aluno1.nome); */
-    /* printf("O cpf do aluno eh %s\n",aluno1.cpf); */
-    /* printf("O telefone do aluno eh %s\n",aluno1.telefone); */
-    /* printf("O email do aluno eh %s\n",aluno1.email); */
-
     return 0;
 }
-
-/* void cadastroAluno(struct aluno *cadastro){*/
-/* 	printf("<------- CADASTRO DO ALUNO ------->\n");*/
-
-/* 	printf("digite o nome do aluno:");*/
-/* 	gets(cadastro->nome); // ponteiro*/
-
-/* 	printf("digite o Cpf do aluno:");*/
-
-/* 	gets(cadastro->cpf); // ponteiro*/
-
-/* 	printf("digite o Telefone do aluno:");*/
-
-/* 	gets(cadastro->telefone); // ponteiro*/
-
-/* 	printf("digite o Email do aluno:");*/
-
-/* 	gets(cadastro->email); // ponteiro*/
-
-/* 	 talvez usar getchar(); a partir do segundo gets*/
-/* }*/
