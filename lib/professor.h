7382057f32b398498bc0436a83f7bb20519b13eb
Joao Bartolot (windows) <jvcbartolot@gmail.com>
2019-11-05 00:32:57 -0300

    [7] - Ajustes da funcao system(clear)

--- lib/professor.h
+++ lib/professor.h
@@ -4,8 +4,8 @@
 #ifdef _WIN32        // Caso o sistema operacional seja windows
 #define clear "cls"  // o clear vai ser o comando cls
 #endif
-#ifdef defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))  // Caso seja unix like
-#define clear "clear"                                                                                      // o clear sera o comando clear
+#if defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))  // Caso seja unix like
+#define clear "clear"                                                                                   // o clear sera o comando clear
 #endif
 
 struct professor {
